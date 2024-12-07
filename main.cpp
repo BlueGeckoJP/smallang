77d35f6a2af77b6a52ba8c6bac4b3c86ae5c45c9
bluegecko <kei09doya@gmail.com>
2024-12-07 19:35:16 +0900

    implemented lexer

--- main.cpp
+++ main.cpp
@@ -33,17 +33,25 @@ class Lexer {
     while (this->position < static_cast<int>(this->input_text.size())) {
       char current_char = this->input_text[this->position];
 
-      // TODO: Implement this
       switch (this->current_state) {
         case State::INITIAL:
+          this->handle_initial_state(current_char);
+          break;
         case State::NUMBER:
+          this->handle_number_state(current_char);
+          break;
         case State::IDENTIFIER:
+          this->handle_identifier_state(current_char);
+          break;
         case State::OPERATOR:
+          this->handle_operator_state();
           break;
       };
 
       this->position++;
     }
+
+    this->finalize_current_token();
   }
 
   void handle_initial_state(char c) {
@@ -68,9 +76,57 @@ class Lexer {
     if (isdigit(c)) {
       this->current_token_value += c;
     } else {
-      // TODO: Implement finalize current token
+      this->finalize_current_token();
+      this->current_state = State::INITIAL;
+      this->position--;
+    }
+  }
+
+  void handle_identifier_state(char c) {
+    if (isalnum(c)) {
+      this->current_token_value += c;
+    } else {
+      this->finalize_current_token();
+      this->current_state = State::INITIAL;
+      this->position--;
+    }
+  }
+
+  void handle_operator_state() {
+    this->finalize_current_token();
+    this->current_state = State::INITIAL;
+    this->position--;
+  }
+
+  void finalize_current_token() {
+    std::string token_type = this->determine_token_type();
+    this->tokens.push_back(Token(token_type, this->current_token_value));
+    this->current_token_value = "";
+  }
+
+  std::string determine_token_type() {
+    switch (this->current_state) {
+      case State::NUMBER:
+        return "NUMBER";
+      case State::IDENTIFIER:
+        return "IDENTIFIER";
+      case State::OPERATOR:
+        return "OPERATOR";
+      default:
+        return "UNKNOWN";
+    }
+  }
+
+  void debug_print_tokens() {
+    for (auto it = this->tokens.begin(); it != this->tokens.end(); ++it) {
+      std::cout << "Type: " << it->type << ", Value:" << it->value << "\n";
     }
   }
 };
 
-int main(int, char**) { std::cout << "Hello, from smallang!\n"; }
+int main(int, char**) {
+  std::string input_text = "add 123 + 456";
+  Lexer lexer = Lexer(input_text);
+  lexer.tokenize();
+  lexer.debug_print_tokens();
+}
